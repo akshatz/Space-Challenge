a23449979a1082224f0067ae3f668c2c2a88ca41
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2018-12-18 15:02:01 +0530

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Simulation {
     ArrayList<Item> loadItemsOne() throws Exception {
         ArrayList<Item> loadManifest = new ArrayList();
-        Scanner fileScanner = new Scanner(new File("C:\\Users\\User2\\Desktop\\ud282-master\\SpaceChallange\\src\\phase-1.txt"));
+        Scanner fileScanner = new Scanner(new File("phase-1.txt"));
         while (fileScanner.hasNextLine()){
             Item newItem = new Item();
             String[] tokens = fileScanner.nextLine().split("=");
@@ -16,7 +16,7 @@ public class Simulation {
     }
     ArrayList<Item> loadItemsTwo() throws Exception {
         ArrayList<Item> loadManifest = new ArrayList();
-        Scanner fileScanner = new Scanner(new File("C:\\Users\\User2\\Desktop\\ud282-master\\SpaceChallange\\src\\phase-2.txt"));
+        Scanner fileScanner = new Scanner(new File("phase-2.txt"));
         while (fileScanner.hasNextLine()) {
             Item newItem = new Item();
             String[] tokens = fileScanner.nextLine().split("=");
