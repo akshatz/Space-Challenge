99d196ff4a0ae3126b97aa23d59222f05545b163
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2019-01-09 22:12:40 +0530

    Update U2.java

--- U2.java
+++ U2.java
@@ -1,20 +1,21 @@
+import java.util.Random;
+
 import static java.lang.Math.abs;
 
 class U2 extends Rocket {
     U2()
     {
         cost = 120000000;
-        currentWeight = 180000;
-        maxWeight = 29000;
+        cargo_carried = 180000;
+        max_cargo = 29000;
     }
     public boolean launch()
     {
-        int randNo = abs((int) Math.floor(Math.random()*100));
-
-        return (4*(currentWeight / maxWeight) < randNo);
+        int randNo = abs((int) Math.floor(Math.random()*101));
+        return (4*(cargo_carried/ max_cargo) < randNo);
     }
     public boolean land() {
-        int randNo = abs((int) Math.floor(Math.random()*100));
-        return (8*(currentWeight / maxWeight) < randNo);
+        int randNo = abs((int) Math.floor(Math.random()*101));
+        return (8*( cargo_carried / max_cargo) < randNo);
     }
 }
