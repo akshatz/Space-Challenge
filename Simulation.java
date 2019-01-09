609831e9ae31cdc48304341ac45a2fb384d325d3
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2019-01-09 22:14:41 +0530

    Update Simulation.java

--- Simulation.java
+++ Simulation.java
@@ -1,66 +1,80 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.io.File;
 import java.util.Scanner;
+
 public class Simulation {
-    ArrayList<Item> loadItemsOne() throws Exception {
-        ArrayList<Item> loadManifest = new ArrayList();
-        Scanner fileScanner = new Scanner(new File("phase-1.txt"));
+    ArrayList<Item> loadItemsOne() throws IOException {
+        ArrayList<Item> loadManifest = new ArrayList<>();
+        try{
+        Scanner fileScanner = new Scanner(new File("C:\\Users\\User2\\Desktop\\ud282-master\\SpaceChallange\\src\\phase-1.txt"));
         while (fileScanner.hasNextLine()){
             Item newItem = new Item();
             String[] tokens = fileScanner.nextLine().split("=");
             newItem.name = tokens[0];
             newItem.weight = Integer.parseInt(tokens[tokens.length-1]);
             loadManifest.add(newItem);
+        }}
+        catch(IOException e){
+            e.printStackTrace();
         }
         return loadManifest;
     }
-    ArrayList<Item> loadItemsTwo() throws Exception {
-        ArrayList<Item> loadManifest = new ArrayList();
-        Scanner fileScanner = new Scanner(new File("phase-2.txt"));
-        while (fileScanner.hasNextLine()) {
-            Item newItem = new Item();
-            String[] tokens = fileScanner.nextLine().split("=");
-            newItem.name = tokens[0];
-            newItem.weight = Integer.parseInt(tokens[tokens.length-1]);
-            loadManifest.add(newItem);
+    ArrayList<Item> loadItemsTwo() throws IOException {
+        ArrayList<Item> loadManifest = new ArrayList<>();
+        try{
+            Scanner fileScanner = new Scanner(new File("C:\\Users\\User2\\Desktop\\ud282-master\\SpaceChallange\\src\\phase-2.txt"));
+            while (fileScanner.hasNextLine()){
+                Item newItem = new Item();
+                String[] tokens = fileScanner.nextLine().split("=");
+                newItem.name = tokens[0];
+                newItem.weight = Integer.parseInt(tokens[tokens.length-1]);
+                loadManifest.add(newItem);
+            }}
+        catch(IOException e){
+            e.printStackTrace();
         }
         return loadManifest;
     }
-    ArrayList<Rocket> loadU1(ArrayList<Item> list) {
-        ArrayList<Rocket> rocketsOne = new ArrayList();
+
+    public ArrayList<Rocket> loadU1(ArrayList<Item> list) {
+        ArrayList<Rocket> rocketsOne = new ArrayList<>();
         Rocket newOne = new U1();
         for (Item thisItem : list) {
-            if (newOne.canCarry(thisItem)) {
-                rocketsOne.add(newOne);
-            } else {
+            if(newOne.carry(thisItem)){
+                if (newOne.canCarry(thisItem)) {
+                    rocketsOne.add(newOne);
+                }
+            }
+            else {
                 newOne = new U1();
                 rocketsOne.add(newOne);
-            }
-        }
-        return rocketsOne;
+             }
+         }
+         return rocketsOne;
     }
     ArrayList<Rocket> loadU2(ArrayList<Item> list) {
-        ArrayList<Rocket> rocketsTwo = new ArrayList();
-        Rocket newTwo = new U2();
-        for (Item thisItem : list) {
-            if (newTwo.canCarry(thisItem)) {
-                rocketsTwo.add(newTwo);
-            } else {
-                newTwo = new U2();
-                rocketsTwo.add(newTwo);
-            }
-
-        }
-        return rocketsTwo;
-    }
+         ArrayList<Rocket> rocketsTwo = new ArrayList<>();
+         Rocket newTwo = new U2();
+         for (Item thisItem : list) {
+             if(newTwo.carry(thisItem)){
+                 if (newTwo.canCarry(thisItem)) {
+                     rocketsTwo.add(newTwo);
+                 }
+             }
+             else {
+                 rocketsTwo.add(newTwo);
+             }
+         }
+         return rocketsTwo;
+     }
     int runSimulation(ArrayList<Rocket> rockets) {
-        int totalCost = 0;
-        for (Rocket currentRocket : rockets) {
-            totalCost += currentRocket.getCost();
-            while (!currentRocket.launch() || !currentRocket.land()) {
-                totalCost += currentRocket.getCost();
-            }
-        }
-        return totalCost;
+         int totalCost = 0;
+         for (Rocket currentRocket : rockets) {
+             totalCost += currentRocket.getCost();
+             while (!currentRocket.launch() || !currentRocket.land()) {
+                 totalCost += currentRocket.getCost();
+             }
+         }
+         return totalCost;
     }
 }
