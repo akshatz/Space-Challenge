43c5a8e5daaaf731c11b2fd576bc0b6a6c44e2a8
Akshat Zala <30282789+akshatz@users.noreply.github.com>
2019-01-15 11:01:18 +0530

    Update U1.java

--- U1.java
+++ U1.java
@@ -2,8 +2,8 @@ import static java.lang.Math.abs;
 public class U1 extends Rocket{
     U1() {
         cost = 100000000;
-        currentWeight = 10000;
-        maxWeight = 18000;
+        cargo_carried = 10000;
+        max_cargo = 18000;
     }
     public boolean launch() {
         int randNo = abs((int) Math.floor(Math.random() * 100));
